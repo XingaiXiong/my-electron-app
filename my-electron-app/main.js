a7ee0a07c561b98a2c51145da55de978d26e11ba
熊兴艾 <10681599+xiong-xingai@user.noreply.gitee.com>
2026-04-05 22:18:42 +0800

    feat:1.building your first app;2.using preload scripts.

--- my-electron-app/main.js
+++ my-electron-app/main.js
@@ -1,1 +1,31 @@
-console.log('hello from electron')
+const {app, BrowserWindow, ipcMain} = require('electron/main')//Importing two Electron modules;app controls your application's event lifecycle; BrowserWindow creates and manages app windows.
+
+const path = require('node:path')
+
+const createWindow = () => {//This function loads your web page into a new BrowserWindow instance.
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences:{
+            preload: path.join(__dirname, 'preload.js')
+        }
+        })
+        win.loadFile('index.html')
+}
+
+app.whenReady().then(()=>{ //calling your function when the app is ready
+    ipcMain.handle('ping', ()=>'pong')
+    createWindow()
+    app.on('activate',()=>{
+        if(BrowserWindow.getAllWindows().length===0){
+            createWindow()//For macOS, as the application may continue running in the background after the main process exits, launch the application using the following method
+        }
+    })
+})
+
+app.on('window-all-closed',()=>{
+    if(process.platform !== 'darwin'){
+        app.quit()
+    }
+}
+)
